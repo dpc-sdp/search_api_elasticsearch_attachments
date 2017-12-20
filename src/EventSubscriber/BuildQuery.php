55238cf7c12c0614721ec066caff81e472483f04
dakku <dakku@97634.no-reply.drupal.org>
2017-12-20 11:46:24 +0000

    Issue #2932003: Allow option for altering SearchBuilder:build $params

---
+++ src/EventSubscriber/BuildQuery.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\search_api_elasticsearch_attachments\EventSubscriber;
+
+use Drupal\elasticsearch_connector\Event\BuildSearchParamsEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * {@inheritdoc}
+ */
+class BuildSearchParams implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[BuildSearchParamsEvent::BUILD_QUERY] = 'buildSearchParams';
+    return $events;
+  }
+
+  /**
+   * Method to build Params.
+   *
+   * @param \Drupal\elasticsearch_connector\Event\BuildSearchParamsEvent $event
+   *   The BuildSearchParamsEvent event.
+   */
+  public function buildSearchParams(BuildSearchParamsEvent $event) {
+    $params = $event->getElasticSearchParams();
+    $params['body']['highlight']['fields']['es_attachment.content'] = (object)[];
+    $event->setElasticSearchParams($params);
+  }
+
+}

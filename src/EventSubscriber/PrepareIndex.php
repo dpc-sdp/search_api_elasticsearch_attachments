e31b9e632a8e620785db5f61695aec6de5e66671
dakku <dakku@97634.no-reply.drupal.org>
2017-12-20 17:19:32 +0000

    Code cleanup

--- src/EventSubscriber/PrepareIndex.php
+++ src/EventSubscriber/PrepareIndex.php
@@ -2,10 +2,8 @@
 
 namespace Drupal\search_api_elasticsearch_attachments\EventSubscriber;
 
-//use Drupal\elasticsearch_connector\ElasticSearch\Parameters\Factory\IndexFactory;
 use Drupal\elasticsearch_connector\Event\PrepareIndexEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Drupal\search_api\Entity\Index;
 
 
 /**
@@ -24,20 +22,10 @@ class PrepareIndex implements EventSubscriberInterface {
   /**
    * Method to prepare index.
    *
-   * @param Drupal\elasticsearch_connector\Event\PrepareIndexEvent $event
+   * @param \Drupal\elasticsearch_connector\Event\PrepareIndexEvent $event
    *   The PrepareIndexEvent event.
    */
   public function prepareIndex(PrepareIndexEvent $event) {
-//    $indexConfig = $event->getIndexConfig();
-////ksm($indexConfig);
-//    // See IndexFactory:getIndexName()
-//    $options = \Drupal::database()->getConnectionOptions();
-//    $site_database = $options['database'];
-//    $indexName = str_replace('elasticsearch_index_' . $site_database . '_', '', $event->getIndexName());
-//    //ksm(Index::load($indexName));
-//
-//    $indexConfig['body'][$indexName]['properties']['es_attachment']['fields']['content']['store'] = true;
-//    $event->setIndexConfig($indexConfig);
   }
 
 }

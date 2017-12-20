5a9efef64f183fd05c363e75e0992b57bdecbe8a
dakku <dakku@97634.no-reply.drupal.org>
2017-12-20 00:36:23 +0000

    Prepare Mapping added

---
+++ src/EventSubscriber/PrepareMapping.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\search_api_elasticsearch_attachments\EventSubscriber;
+
+use Drupal\elasticsearch_connector\Event\PrepareMappingEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * {@inheritdoc}
+ */
+class PrepareMapping implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[PrepareMappingEvent::PREPARE_MAPPING] = 'prepareMapping';
+    return $events;
+  }
+
+  /**
+   * Method to prepare query.
+   *
+   * @param \Drupal\elasticsearch_connector\Event\PrepareMappingEvent $event
+   *   The PrepareMappingEvent event.
+   */
+  public function prepareMapping(PrepareMappingEvent $event) {
+    $mappingConfig = $event->getMappingConfig();
+    $type = $event->getMappingType();
+    if ($type == 'attachment') {
+      $mappingConfig['fields'] = [
+        "content" => [
+          "store" => TRUE,
+        ]
+      ];
+      //$event->setMappingConfig($mappingConfig);
+    }
+
+
+  }
+
+}
